8d003efecf80b5a1ea1fe0e5f8a06774bd11e5d6
Elena <elenaharan@gmail.com>
2024-03-22 07:31:43 +0000

    Clean up side effect

--- src/App.js
+++ src/App.js
@@ -309,6 +309,10 @@ function MovieDetails({
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
